f05635690766e80f9cab164a5384866ee693e1bb
Johna Biju John <89513064+jbijujohn@users.noreply.github.com>
2026-02-02 10:43:44 +1030

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+const message = document.getElementById("message");
+const question = document.getElementById("question");
 
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "YAYYY!!! 🥰💖";
+  message.innerHTML = "I knew you'd say yes 😘 Happy Valentine’s Day!";
+  noBtn.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
